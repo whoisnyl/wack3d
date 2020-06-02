b405e281d0d48120b13870b6db28f3c7d2275a78
Enyel Cuadro <helloenyel@gmail.com>
2020-06-02 21:57:58 +0800

    updated bg color and removed icon

--- components/utils/loading.js
+++ components/utils/loading.js
@@ -1,10 +1,9 @@
 export default function Loading({ status, overlay }) {
   return (
     <div
-      className={`loading ${status ? "show" : ""}`}
-      style={{ backgroundColor: overlay ? "rgba(0,0,0,0.65)" : "transparent" }}
-    >
-      <img src="/images/logo-icon-medium.png" alt="WACK3D" />
-    </div>
+      className={`loading ${status ? "show" : ""} ${
+        overlay ? "bg-dark" : "bg-transparent"
+      }`}
+    />
   );
 }
